d1b395dcf6da9ad5b977848507f1888cd6e55e8c
GavriloDrljaca <drljaca93@gmail.com>
2015-07-29 14:03:29 +0200

    Employee dialog

--- HRTool/src/main/webapp/app/controllers/employeeController.js
+++ HRTool/src/main/webapp/app/controllers/employeeController.js
@@ -2,3 +2,16 @@
  * Employee controller
  * created by: Gavrilo Drljaca
  */
+
+
+app.controller('employeeController', function ($rootScope, $scope, $window, employeeFactory){
+	
+	
+	$scope.getEmployee() = function(id){
+		$scope.currentEmp = employeeFactory.getEmployee(id)
+		
+		
+	}
+	
+	
+});
--- HRTool/src/main/webapp/app/services/employeeService.js
+++ HRTool/src/main/webapp/app/services/employeeService.js
@@ -3,3 +3,14 @@
  * created by: Gavrilo Drljaca
  */
 
+app.factory('employeeFactory', function($http){
+	
+	var factory = {};
+	
+	factory.getEmployee = function( id ){
+		
+	}
+	factory.saveEmployee = function(emp){
+		// return $http.post('')
+	}
+});
