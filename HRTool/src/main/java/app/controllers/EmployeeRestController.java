a502c183eee050aaf79f11d8014f804afdc72938
Smiley10 <danger.place@hotmail.com>
2015-07-30 12:51:19 +0200

    save employee RestController

--- HRTool/src/main/java/app/controllers/EmployeeRestController.java
+++ HRTool/src/main/java/app/controllers/EmployeeRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -64,4 +65,8 @@ public class EmployeeRestController {
 		return this.projectRepository.findAll(idProjects);
 	}
 	
+	@RequestMapping("/saveemployee")
+	public void saveEmployee(@RequestParam Employee e) {
+		employeeRepository.save(e);
+	}
 }
