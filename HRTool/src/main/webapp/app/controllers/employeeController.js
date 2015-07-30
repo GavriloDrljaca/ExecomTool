c5c44ebb9e7ca868f59084711902cf740e0f7591
GavriloDrljaca <drljaca93@gmail.com>
2015-07-30 11:43:30 +0200

    Date

--- HRTool/src/main/webapp/app/controllers/employeeController.js
+++ HRTool/src/main/webapp/app/controllers/employeeController.js
@@ -2,47 +2,41 @@
  * Employee controller created by: Gavrilo Drljaca
  */
 
+app.controller('employeeController', function($rootScope, $scope, $window,
+		$mdDialog, selectedEmployee, $filter) {
+	function init() {
 
-app.controller('employeeController', function ($rootScope, $scope, $window, $mdDialog, selectedEmployee, $filter){
-	function init(){
-		
-			$scope.activeForm = "none";
-		}
-		
-	
-	 $scope.activeForm = "none";
-	 $scope.currEmp = selectedEmployee;
-	 
-	 
-	 //Date d = new Date("dd-MM-YYYY");
-	 date = Date(parseInt(selectedEmployee.dateOfBirth));
-	 //d.format = "dd-MM-YYYY";
-	 //d.toLocalFormat("dd-MM-YYYY")
-	 date = $filter('date')(date, "yyyy-MM-dd");
-	 $scope.dateAsString = $filter('date')(date, "yyyy-MM-dd");
-	// d = Date(day, month, year)
-	 alert(date);
-	 alert($scope.dateAsString);
-	 $scope.hide = function() {
-		    $mdDialog.hide();
-		  };
-		  $scope.cancel = function() {
-		    $mdDialog.cancel();
-		  };
-		  $scope.answer = function(answer) {
-		    $mdDialog.hide(answer);
-		  }
-	
-     $scope.showForm = function(forma){
-    	 $scope.activeForm = forma;
-     }
-     /*
-      * function toJSONLocal (date) {
-	    var local = new Date(date);
-	    local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-	    return local.toJSON().slice(0, 10);
+		$scope.activeForm = "none";
 	}
-      */
-     
-		  
-	});
+
+	$scope.activeForm = "none";
+	$scope.currEmp = selectedEmployee;
+
+	dat = new Date(selectedEmployee.dateOfBirth);
+
+	$scope.dateOfBirth = $filter('date')(dat, 'yyyy-MM-dd');
+	$scope.dateBirth = new Date($scope.dateOfBirth);
+	alert($scope.dateBirth);
+	$scope.hide = function() {
+		$mdDialog.hide();
+	};
+	$scope.cancel = function() {
+		$mdDialog.cancel();
+	};
+	$scope.answer = function(answer) {
+		$mdDialog.hide(answer);
+	}
+
+	$scope.showForm = function(forma) {
+		$scope.activeForm = forma;
+	}
+	$scope.showDate = function(bla){
+		alert($scope.dateBirth);
+	}
+	/*
+	 * function toJSONLocal (date) { var local = new Date(date);
+	 * local.setMinutes(date.getMinutes() - date.getTimezoneOffset()); return
+	 * local.toJSON().slice(0, 10); }
+	 */
+
+});
