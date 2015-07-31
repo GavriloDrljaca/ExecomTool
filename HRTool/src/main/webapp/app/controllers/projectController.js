483704a04e328b4c8951425b14679bbfee1c10e3
kecman91 <csullog91@gmail.com>
2015-07-31 15:19:51 +0200

    Project Dialog/Controller 1.0

--- HRTool/src/main/webapp/app/controllers/projectController.js
+++ HRTool/src/main/webapp/app/controllers/projectController.js
@@ -1,13 +1,38 @@
-/**
- * 
- */
-app.controller('projectController', function($scope, $window, $mdDialog, selectedProject, projectInfoService){
+app.controller('projectController', function($http, $scope, $window, $mdDialog, selectedProject, projectInfoService){
 		
 	$scope.selectedProject = selectedProject;	
 	 
 	$scope.init = function() {
-		projectInfoService.getEmployeeForProject(selectedProject.idProject).success(function (data) {
-			$scope.employees = data;
+		$scope.employees = [];
+		getEmployees();	
+	}
+	
+	function getEmployees() {
+		
+		var projInfos = {};
+		$http.get(selectedProject._links.projectInfo.href).success(function (data) {
+			if(data._embedded != undefined) {
+				projInfos = data._embedded.projectInfoes;
+			} else {
+				projInfos = {};
+			}
+			temp =[];
+			counter = 0;
+			for(i = 0; i<projInfos.length; i++) {
+				temp.push(projInfos[i]);
+				$http.get(projInfos[i]._links.employee.href).success(function (data) {
+					$scope.employees.push(data);
+					test(data, temp[counter]);
+					counter++;
+				});
+			}
+		});
+	}
+	function test(employee, projInfo){
+		console.log(projInfo);
+		$http.get(employee._links.projectInfos.href).success(function (data) {
+			employee.resp = projInfo.jobResponsibilities;
+			employee.prExp = projInfo.projectExp;
 		});
 	}
 	
