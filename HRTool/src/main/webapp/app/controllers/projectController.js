a3d1569211b1cb1a8fa6baaa87879b6f589e642e
kecman91 <csullog91@gmail.com>
2015-07-31 16:39:03 +0200

    projectDialog izgled

--- HRTool/src/main/webapp/app/controllers/projectController.js
+++ HRTool/src/main/webapp/app/controllers/projectController.js
@@ -9,30 +9,18 @@ app.controller('projectController', function($http, $scope, $window, $mdDialog, 
 	
 	function getEmployees() {
 		
-		var projInfos = {};
+		$scope.projInfos = {};
 		$http.get(selectedProject._links.projectInfo.href).success(function (data) {
 			if(data._embedded != undefined) {
-				projInfos = data._embedded.projectInfoes;
+				$scope.projInfos = data._embedded.projectInfoes;
 			} else {
-				projInfos = {};
+				$scope.projInfos = {};
 			}
-			temp =[];
-			counter = 0;
-			for(i = 0; i<projInfos.length; i++) {
-				temp.push(projInfos[i]);
-				$http.get(projInfos[i]._links.employee.href).success(function (data) {
+			for(i = 0; i<$scope.projInfos.length; i++) {
+				$http.get($scope.projInfos[i]._links.employee.href).success(function (data) {
 					$scope.employees.push(data);
-					test(data, temp[counter]);
-					counter++;
 				});
 			}
-		});
-	}
-	function test(employee, projInfo){
-		console.log(projInfo);
-		$http.get(employee._links.projectInfos.href).success(function (data) {
-			employee.resp = projInfo.jobResponsibilities;
-			employee.prExp = projInfo.projectExp;
 		});
 	}
 	
