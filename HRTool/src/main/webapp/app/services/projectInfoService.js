ccb330da91254996212d2d2800bfa4921c57b8e3
kecman91 <csullog91@gmail.com>
2015-07-31 15:26:07 +0200

    projectInfoService

--- HRTool/src/main/webapp/app/services/projectInfoService.js
+++ HRTool/src/main/webapp/app/services/projectInfoService.js
@@ -2,14 +2,19 @@
  * 
  */
 app.service('projectInfoService', function($http) {
-	
-	return{	
-		/*getEmployeesForProject: function(idProject) {
-			return $http.get('/projectinfo/projectemployee', [idProject])
-		},*/
-	
-		getProjectInfosForProject: function(id) {
-			return $http.get('/projectInfosForProject', [id])
+	url = '/projectInfoes';
+	return{
+		create: function(projectInfo){
+			return $http.post(url, projectInfo)
+		},
+		update: function(projectInfo){
+			return $http.put(projectInfo._links.self.href, projectInfo)
+		},
+		list: function(){
+			return $http.get(url);
+		},
+		delete: function(projectInfo){
+			return $http.delete(projectInfo._links.self.href)
 		}
-	}	
+	}
 });
