f82014fcfe08293540da3175a99fcfec2d41733d
kecman91 <csullog91@gmail.com>
2015-08-03 13:00:57 +0200

    Project Dialog

--- HRTool/src/main/webapp/app/controllers/projectController.js
+++ HRTool/src/main/webapp/app/controllers/projectController.js
@@ -1,14 +1,15 @@
-app.controller('projectController', function($http, $scope, $window, $mdDialog, selectedProject, projectInfoService){
+app.controller('projectController', function($http, $scope, $mdDialog, selectedProject, employeeService){
 		
 	$scope.selectedProject = selectedProject;	
 	 
 	$scope.init = function() {
-		$scope.employees = [];
-		getEmployees();	
+		getEmployees();
+		getAllEmployees();
 	}
 	
 	function getEmployees() {
-		
+
+		$scope.employees = [];
 		$scope.projInfos = {};
 		$http.get(selectedProject._links.projectInfo.href).success(function (data) {
 			if(data._embedded != undefined) {
@@ -22,6 +23,19 @@ app.controller('projectController', function($http, $scope, $window, $mdDialog, 
 				});
 			}
 		});
+	}
+	
+	function getAllEmployees() {
+		employeeService.list().then(function(data) {
+			$scope.otherEmployees = data.data._embedded.employees;
+			for(i=0; i<$scope.employees.lenght; i++) {
+				removable = $scope.otherEmployees.indexOf($scope.employees[i]);
+				if(removable>0) {
+					$scope.otherEmployees.splice(removable,1);
+				};
+			};
+			console.log($scope.otherEmployees);
+		});		
 	}
 	
 	$scope.answer = function(answer) {
