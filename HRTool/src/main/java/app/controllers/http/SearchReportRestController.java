d6817c6c4214540362aefcf87914a2040e585f82
praktikant3 <praktikant3@praktikant3>
2015-08-07 15:30:58 +0200

    Report gotov osnovna verzija

--- HRTool/src/main/java/app/controllers/http/SearchReportRestController.java
+++ HRTool/src/main/java/app/controllers/http/SearchReportRestController.java
@@ -14,6 +14,8 @@ import app.model.EmploymentInfo;
 import app.model.ProjectInfo;
 import app.model.SearchReport;
 import app.model.SeniorityEnum;
+import app.model.TagCloud;
+import app.model.TagCloudEnum;
 import app.repository.EmployeeRepository;
 import app.repository.ProjectInfoRepository;
 import app.repository.ProjectRepository;
@@ -43,19 +45,24 @@ public class SearchReportRestController {
 		for (Employee emp : allEmployees){
 			boolean addEmployee = false;
 			projectInfoes = emp.getProjectInfos();
-			for (ProjectInfo pi : projectInfoes){
-				if (sr.getSeniority() != null){
-					if (SeniorityEnum.valueOf(sr.getSeniority()) == pi.getSeniority()){
+			if (projectInfoes.size()==0 && sr.getSeniority() == null){
+				addEmployee = true;
+			}
+			else{
+				for (ProjectInfo pi : projectInfoes){
+					if (sr.getSeniority() != null){
+						if (SeniorityEnum.valueOf(sr.getSeniority()) == pi.getSeniority()){
+							addEmployee = true;
+							break;
+						}else {
+							if (addEmployee){
+								addEmployee = false;
+							}
+						}
+					}else {
 						addEmployee = true;
 						break;
-					}else {
-						if (addEmployee){
-							addEmployee = false;
-						}
 					}
-				}else {
-					addEmployee = true;
-					break;
 				}
 			}
 			
@@ -96,20 +103,145 @@ public class SearchReportRestController {
 			}
 			if (!addEmployee) continue;
 			
+			if (sr.getYearOfEmploymentRelation() == null){
+				sr.setYearOfEmploymentRelation("After");
+			}
 			employmentInfoes = emp.getEmpInfos();
-			if (addEmployee){
+			if (employmentInfoes.size() == 0 && sr.getYearOfEmployment()!=null){
+				addEmployee = false;
+			}
+			else if (sr.getYearOfEmployment() !=null){
 				for (EmploymentInfo ei : employmentInfoes){
-					if (ei.getCompanyName().toLowerCase().equals("execom") && sr.getYearOfEmployment() != null){
-						if (ei.getStartDate().after(sr.getYearOfEmployment())){
-							addEmployee = true;
-							break;
-						}else{
-							if (addEmployee){
-								addEmployee = false;
-							}
+					if (ei.getCompanyName().toLowerCase().equals("execom")){
+						switch (sr.getYearOfEmploymentRelation()){
+							case "Before":
+								if (ei.getStartDate().before(sr.getYearOfEmployment())){
+									addEmployee = true;
+									break;
+								}else{
+									if (addEmployee){
+										addEmployee = false;
+									}
+								}
+								break;
+							case "After":
+								if (ei.getStartDate().after(sr.getYearOfEmployment())){
+									addEmployee = true;
+									break;
+								}else{
+									if (addEmployee){
+										addEmployee = false;
+									}
+								}
+								break;
 						}
 					}
+					if(!addEmployee){
+						break;
+					}
+				}
+			}
+			if (!addEmployee) continue;
+
+			int count = 0;
+			boolean stop;
+			for (String tech : sr.getTechnology()){
+				stop = false;
+				for(ProjectInfo pi : projectInfoes){
+					for (TagCloud tg : pi.getTagClouds()){
+						if (tg.getTipTagCloud().equals(TagCloudEnum.Technologie) && tech.equals(tg.getNameTagCloud())){
+							count++;
+							stop=true;
+							break;
+						}
+					}
+					if (stop){
+						break;
+					}
 				}	
+			}
+			if (count == sr.getTechnology().size()){
+				addEmployee = true;
+			}else{
+				addEmployee = false;
+			}
+			if (!addEmployee) continue;
+			
+			count = 0;
+			for (String db : sr.getDatabase()){
+				stop = false;
+				for(ProjectInfo pi : projectInfoes){
+					for (TagCloud tg : pi.getTagClouds()){
+						if (tg.getTipTagCloud().equals(TagCloudEnum.Database) && db.equals(tg.getNameTagCloud())){
+							count++;
+							stop=true;
+							break;
+						}
+					}
+					if (stop){
+						break;
+					}
+				}		
+			}
+			if (count == sr.getDatabase().size()){
+				addEmployee = true;
+			}else{
+				addEmployee = false;
+			}
+			if (!addEmployee) continue;
+			
+			count = 0;
+			for (String edu : sr.getEducation()){
+				for (TagCloud tg : emp.getTagClouds()){
+					if (tg.getTipTagCloud().equals(TagCloudEnum.Education) && edu.equals(tg.getNameTagCloud())){
+						count++;
+						break;
+					}
+				}
+			}
+			if (count == sr.getEducation().size()){
+				addEmployee = true;
+			}else{
+				addEmployee = false;
+			}
+			if (!addEmployee) continue;
+			
+			count = 0;
+			for (String lang : sr.getLanguage()){
+				for (TagCloud tg : emp.getTagClouds()){
+					if (tg.getTipTagCloud().equals(TagCloudEnum.ForeignLanguage) && lang.equals(tg.getNameTagCloud())){
+						count++;
+						break;
+					}
+				}
+			}
+			if (count == sr.getLanguage().size()){
+				addEmployee = true;
+			}else{
+				addEmployee = false;
+			}
+			if (!addEmployee) continue;
+			
+			count = 0;
+			for (String pos : sr.getPosition()){
+				stop = false;
+				for(ProjectInfo pi : projectInfoes){
+					for (TagCloud tg : pi.getTagClouds()){
+						if (tg.getTipTagCloud().equals(TagCloudEnum.Position) && pos.equals(tg.getNameTagCloud())){
+							count++;
+							stop=true;
+							break;
+						}
+					}
+					if (stop){
+						break;
+					}
+				}		
+			}
+			if (count == sr.getPosition().size()){
+				addEmployee = true;
+			}else{
+				addEmployee = false;
 			}
 			if (!addEmployee) continue;
 			
