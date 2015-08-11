dcb9e5e747965aa95c269961f8222f632d666d0e
Smiley10 <danger.place@hotmail.com>
2015-08-11 15:33:06 +0200

    test

--- HRTool/src/main/java/app/Application.java
+++ HRTool/src/main/java/app/Application.java
@@ -40,7 +40,7 @@ public class Application implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
-
+	//aaaaaaaaaaaaaaaaaaaaaaaaaa
 	@Override
 	public void run(String... strings) throws Exception {
 		addEmployees();
