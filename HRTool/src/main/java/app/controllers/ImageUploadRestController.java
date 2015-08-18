c857d89da6af641b8a6a94d113981bd830657d58
Smiley10 <danger.place@hotmail.com>
2015-08-18 14:03:08 +0200

    - @ResponseBody + ResponseEntity<>

--- HRTool/src/main/java/app/controllers/ImageUploadRestController.java
+++ HRTool/src/main/java/app/controllers/ImageUploadRestController.java
@@ -5,33 +5,36 @@ import java.io.File;
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
 public class ImageUploadRestController {
 
 	@Autowired
 	private ServletContext servletContext;
 
+	private static String imagePath = "/temp/";
+
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public @ResponseBody String uploadingPOST(
-			@RequestParam("picture") MultipartFile file) {
-		if (!file.isEmpty()) {
+	public ResponseEntity<String> uploadingPOST(
+			@RequestParam("picture") MultipartFile image) {
+		if (!image.isEmpty()) {
 			try {
-				file.transferTo(new File(servletContext.getRealPath("/images")
-						+ file.getOriginalFilename()));
-				return "images/" + file.getOriginalFilename();
+				image.transferTo(new File(servletContext.getRealPath(imagePath)
+						+ image.getOriginalFilename()));
+				return new ResponseEntity<String>(imagePath
+						+ image.getOriginalFilename(), HttpStatus.OK);
 			} catch (Exception e) {
-				return "You failed to upload " + e.getMessage();
+				return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 			}
 		} else {
-			return "You failed to upload  because the file was empty.";
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
 		}
 	}
-
 }
