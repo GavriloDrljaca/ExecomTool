23decbf714051377831176ca17bcf9dd2334bc2d
kecman91 <csullog91@gmail.com>
2015-08-18 12:32:35 +0200

    Login controller

--- HRTool/src/main/webapp/app/app.js
+++ HRTool/src/main/webapp/app/app.js
@@ -5,20 +5,24 @@
 
 var app = angular.module('app', ['ngRoute', 'ngMaterial', 'ng', 'flow']);
 
+app.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/tagCloudTab', {                                               
+	        controller:'tagCloudController',               
+	        templateUrl: 'app/partials/tagCloudsTab.html',
+	    })
+	    .when('/', {
+	    	controller: 'app/controllers/LoginController',
+	    	templateUrl: 'app/partials/login.html',
+	    })
+	    .otherwise({                      
+	        redirectTo: '/'
+	    });      
+}]);
+
 app.filter("jsDate", function () {
     return function (x) {
        // return new Date(parseInt(x.toString().substring(6)));
     	return new Date(parseInt(x));
     };
-});
-
-app.config(function($routeProvider, $locationProvider){
-	$routeProvider
-		.when('/tagCloudTab', {                                            
-	        templateUrl: "app/partials/tagCloudsTab.html",                                               
-	        controller:'tagCloudController',                                
-	       })                                                                      
-	       .otherwise({                      
-	           template: 'does not exists'   
-	       });      
 });
